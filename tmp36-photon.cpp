0d1bd950cd95164cf9a7946fded1d74ec02c3c6a
Brett Ian Balogh <giantmolecules@gmail.com>
2016-06-30 12:01:35 -0500

    Update tmp36-photon.cpp

--- tmp36-photon.cpp
+++ tmp36-photon.cpp
@@ -1,13 +1,17 @@
 Timer tempTimer(2000, publishTemp);
+Servo myServo;
 
 int val = 0;
 float voltage = 0.0;
 float temperatureC = 0.0;
+int servoPos = 0;
 
 void setup() {
     pinMode(A0, INPUT);
+    pinMode(D0, OUTPUT);
     Serial.begin(9600);
     tempTimer.start();
+    myServo.attach(D0);
 }
 
 void loop() {
@@ -16,6 +20,8 @@ void loop() {
     Serial.print("Voltage = ");
     Serial.print(voltage);
     temperatureC = (voltage - 0.5) * 100;
+    servoPos = map(temperatureC, 0, 100, 5,175);
+    myServo.write(servoPos);
     Serial.print(" Temperature = ");
     Serial.print(temperatureC);
     Serial.println(" C");
